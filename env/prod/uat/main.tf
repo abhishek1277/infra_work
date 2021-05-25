0cfe7a219ac20221ba69686bf93c9cd4a039ed6f
Abhishek1277 <abhishekcse98@gmail.com>
2021-05-25 13:30:34 +0530

    Add files via upload

---
+++ env/prod/uat/main.tf
@@ -0,0 +1,29 @@
+# data "terraform_remote_state" "vpc" {
+#   backend = "remote"
+#   config = {
+#     organization = "Prod112"
+#     workspaces = {
+#       name = "infra_work"
+#     }
+#   }
+# }
+provider "azurerm" {
+  version = "~>2.46.0"
+    features {}
+  }
+
+ 
+
+ 
+variable "location_name" {
+  type = string
+  description = "Resource location"
+  default = "westeurope"
+}
+ 
+ 
+resource "azurerm_resource_group" "rg" {
+  name = "uatrg"
+  location = var.location_name
+}
+ 
