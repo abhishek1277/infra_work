fa6557a0d175039e710f8696d96153734fa06540
Abhishek1277 <abhishekcse98@gmail.com>
2021-05-25 13:33:37 +0530

    Update main.tf

--- env/prod/uat/main.tf
+++ env/prod/uat/main.tf
@@ -1,12 +1,12 @@
-# data "terraform_remote_state" "vpc" {
-#   backend = "remote"
-#   config = {
-#     organization = "Prod112"
-#     workspaces = {
-#       name = "infra_work"
-#     }
-#   }
-# }
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    organization = "Prod112"
+    workspaces = {
+      name = "infra_work"
+    }
+  }
+}
 provider "azurerm" {
   version = "~>2.46.0"
     features {}
