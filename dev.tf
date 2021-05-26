e549a62973909fafea4b29bdab53ee34bb3d18b1
Abhishek1277 <abhishekcse98@gmail.com>
2021-05-26 10:35:40 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -7,6 +7,6 @@ resource "azurerm_resource_group" "rg1" {
 resource "azurerm_virtual_network" "example" {
   name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
 }
