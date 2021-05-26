a216f9734601f6ac861522cb7817f46eb7eca173
Abhishek1277 <abhishekcse98@gmail.com>
2021-05-26 10:23:55 +0530

    Update uat..tf

--- uat..tf
+++ uat..tf
@@ -1,29 +1,6 @@
-# data "terraform_remote_state" "vpc" {
-#   backend = "remote"
-#   config = {
-#     organization = "Prod112"
-#     workspaces = {
-#       name = "infra_work"
-#     }
-#   }
-# }
-provider "azurerm" {
-  version = "~>2.46.0"
-    features {}
-  }
 
- 
-
- 
-variable "location_name" {
-  type = string
-  description = "Resource location"
-  default = "westeurope"
-}
- 
- 
 resource "azurerm_resource_group" "rg" {
   name = "uatrg"
-  location = var.location_name
+  location = "westeurope"
 }
  
