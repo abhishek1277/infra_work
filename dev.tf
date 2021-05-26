2df663043d7631f296c3df81ddfa8dd96b845533
Abhishek1277 <abhishekcse98@gmail.com>
2021-05-26 10:23:27 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -1,28 +1,5 @@
-data "terraform_remote_state" "vpc" {
-  backend = "remote"
-  config = {
-    organization = "Prod112"
-    workspaces = {
-      name = "infra_work"
-    }
-  }
-}
-provider "azurerm" {
-  version = "~>2.46.0"
-    features {}
-  }
 
- 
-
- 
-variable "location_name" {
-  type = string
-  description = "Resource location"
-  default = "westeurope"
-}
- 
- 
 resource "azurerm_resource_group" "rg1" {
   name = "devrg"
-  location = var.location_name
+  location = "westeurope"
 }
