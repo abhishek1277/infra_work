9277bf459ae530edb618e88ebd6773a3cab7ec16
Abhishek1277 <abhishekcse98@gmail.com>
2021-07-09 16:04:38 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -31,7 +31,7 @@ resource "azurerm_template_deployment" "example" {
   name                = "acctesttemplate-01"
   resource_group_name = azurerm_resource_group.example.name
 
-  template_body =
+  template_body =<<DEPLOY
 {
   "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
   "contentVersion": "1.0.0.0",
@@ -47,7 +47,8 @@ resource "azurerm_template_deployment" "example" {
     "properties": {
           }
     }
-  ]
+  ],
+  DEPLOY
    deployment_mode = "Incremental"
 }
 
