3e2888670e2ce54b22ce79fc7ee8eb0cd3fd8ff4
Abhishek1277 <abhishekcse98@gmail.com>
2021-07-09 15:59:04 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -31,21 +31,25 @@ resource "azurerm_template_deployment" "example" {
   name                = "acctesttemplate-01"
   resource_group_name = azurerm_resource_group.example.name
 
-  template_body = <<DEPLOY
+  template_body =
 {
   "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
   "contentVersion": "1.0.0.0",
   
-  "resources":[{
+  "resources":[
+    {
       "apiVersion": "2018-11-01",
-      "name": "NewRelic.Azure.WebSites",
+      "name": "NewRelic.Azure.WebSites.Extension",
       "type": "siteextensions",
       "dependsOn": [
         "[resourceId('Microsoft.Web/sites', variables('example-app-service3245'))]"
       ]
-    }]
+    "properties": {
+          }
+    }
+  ]
 }
-DEPLOY
+
 
 
 
