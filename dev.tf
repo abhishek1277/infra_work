a72ba84db2da7469f98153baf509fb30aa66ec9c
Abhishek1277 <abhishekcse98@gmail.com>
2021-07-19 21:43:56 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -26,39 +26,4 @@ resource "azurerm_app_service" "example" {
   }
   }
 
-resource "azurerm_template_deployment" "example" {
-  name                = "acctesttemplate-01"
-  resource_group_name = azurerm_resource_group.example.name
-
-  template_body =<<DEPLOY
-{
-  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
-  "contentVersion": "1.0.0.0",
-  "variables": {
-    "webSiteName": "example-app-service3245"
-  },
-  "parameters": {
-        "sites_example_app_service3245_name": {
-            "defaultValue": "example-app-service3245",
-            "type": "String"
-        },
-        "serverfarms_example_appserviceplan123_externalid": {
-             "defaultValue": "/subscriptions/3457d5d0-1535-40c6-85fb-cb1919104698/resourceGroups/example-resources/providers/Microsoft.Web/serverfarms/example-appserviceplan123",
-            "type": "String"
-        }
-    },
-  "resources":[
-   
-    {
-            "type": "Microsoft.Web/sites/siteextensions",
-            "apiVersion": "2018-11-01",
-            "name": "[concat(parameters('sites_example_app_service3245_name'), '/NewRelic.Azure.WebSites.Extension')]",
-            "location": "West Europe",
-
-    }
-  ]
-}
-DEPLOY
-   deployment_mode = "Incremental"
-}
 
