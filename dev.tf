f03eaff179898556d4dc34a3d77b5addea3ca974
Abhishek1277 <abhishekcse98@gmail.com>
2021-07-19 20:27:08 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -35,6 +35,14 @@ resource "azurerm_template_deployment" "example" {
 {
   "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
   "contentVersion": "1.0.0.0",
+  "parameters": {
+        "sites_example_app_service3245_name": {
+            "value": null
+        },
+        "serverfarms_example_appserviceplan123_externalid": {
+            "value": null
+        }
+    }
   "resources":[
     {
             "type": "Microsoft.Web/sites/siteextensions",
